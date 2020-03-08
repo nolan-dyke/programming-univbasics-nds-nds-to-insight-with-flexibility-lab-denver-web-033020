dee723abbaf6851e28fca8fa2be4e9e9bf668002
Nolan Dyke <nolan.g.dyke@gmail.com>
2020-03-08 22:46:44 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,8 @@
 # Provided, don't edit
 require 'directors_database'
+require 'pp'
+
+pp directors_database
 
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
 # [3,4,5], [6]] => [1,2,3,4,5,6].
@@ -48,6 +51,13 @@ def movies_with_director_key(name, movies_collection)
   # Array of Hashes where each Hash represents a movie; however, they should all have a
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
+  result = []
+  index = 0
+  while index < movies_collection.length do 
+    result << {:director_name => movie_with_director_name(name, movies_collection[index])[:director_name]}
+    index += 1 
+  end 
+  result
 end
 
 
@@ -63,6 +73,18 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
+  result = {} 
+  index = 0 
+  while index < collection.length do
+    studio_key = collection[index][:studio]
+    if result[studio_key]
+      result[studio_key] += collection[index][:worldwide_gross]
+    else 
+      result[studio_key] = collection[index][:worldwide_gross]
+    end 
+    index += 1 
+  end 
+  result
 end
 
 def movies_with_directors_set(source)
@@ -76,6 +98,21 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+  result = []
+  index = 0 
+  while index < source.length do 
+    result << source[index][:movies]
+    index += 1 
+  end
+  result[0].unshift({:director_name => "Byron Poodle"})
+  result_index = 1 
+  source_index = 1 
+  while result_index < result.length do 
+    result[result_index].unshift({:director_name => source[source_index][:name]})
+    result_index += 1 
+    source_index += 1 
+  end 
+  result
 end
 
 # ----------------    End of Your Code Region --------------------
